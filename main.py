d14be57e1d0ee7ab24e8a28727dbb4c79f2cb0b3
Adhii136 <121448026+Adhii136@users.noreply.github.com>
2023-09-01 20:20:29 +0530

    Add files via upload
    Write a program that takes 2 inputs from the user in variable a and b.
    
    Based on these inputs find the output of following expressions:
    
    
    
    1. Floor Division
    
    2. a power b
    
    3. Find out whether two variables have equal value or not - if yes then print True if they are not equal then print False

---
+++ main.py
@@ -0,0 +1,8 @@
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+floor_division_result = a // b
+power_result = a ** b
+are_equal = a == b
+print("1. Floor Division Result:", floor_division_result)
+print("2. Exponentiation Result:", power_result)
+print("3. Are a and b equal?", are_equal)
